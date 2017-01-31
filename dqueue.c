f7e94db383dc3d20d8c2fa92096ac200b2458796
Oskar Tengberg <oste0002@gmail.com>
2017-02-01 00:08:03 +0100

    Add preallocation support

--- dqueue.c
+++ dqueue.c
@@ -1,46 +1,56 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include "dqueue.h"
+#include "prealloc.h"
 
 
 
-dqueue_head *dqueue_init() {
-  dqueue_head *head = (dqueue_head *)calloc(1,sizeof(dqueue_head));
+int dqueue_init(dqueue_head *head) {
+
 
   head->is_head = true;
-  head->is_blocked = false;
+  head->p_head =
+    prealloc_init(PREALLOC_ALLOC_SIZE, PREALLOC_MAX_SIZE, sizeof(dqueue_link));
+  head->p_head_lock = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
   head->push = NULL;
   head->pop = NULL;
-  head->mutex_push = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
-  head->mutex_pop = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
+  head->push_lock = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
+  head->pop_lock = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
 
-  return head;
+  return 0;
 }
 
 
-int dqueue_push(dqueue_head *head, void *data, size_t data_size) {
+int dqueue_push(dqueue_head *head, int32_t num_poppers, void *data,
+    size_t data_size) {
 
-    dqueue_link *push_link = (dqueue_link *)calloc(1,sizeof(dqueue_link));
-    push_link->is_head = false;
-    push_link->mutex_next = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
-    push_link->data_size = data_size;
-    push_link->data = data;
+  prealloc_cell *p_cell = prealloc_new(head->p_head);
+  dqueue_link *push_link = (dqueue_link *) prealloc_memget(p_cell);
+  push_link->prealloc.p_cell = p_cell;
+  push_link->prealloc.p_head = head->p_head;
+  push_link->prealloc.p_lock = &head->p_head_lock;
+  push_link->is_head = false;
+  push_link->next_lock = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
+  push_link->data_size = data_size;
+  push_link->data = data;
+  push_link->num_poppers = num_poppers;
+  push_link->num_poppers_lock = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
 
-    /* It is important to always lock head before the link to avoid
-     * situations where two calling functions block each other */
-    pthread_mutex_lock(&head->mutex_push);
-    pthread_mutex_lock(&head->push->mutex_next);
+  /* It is important to always lock the head before the link to avoid
+   * situations where two calling functions block each other */
+  pthread_mutex_lock(&head->push_lock);
+  pthread_mutex_lock(&head->push->next_lock);
 
-    if (head->push != NULL)
-      head->push->next = push_link;
-    else
-      head->pop = push_link;
+  if (head->push != NULL)
+    head->push->next = push_link;
+  else
+    head->pop = push_link;
+  head->push = push_link;
 
-    head->push = push_link;
-
-    pthread_mutex_unlock(&head->mutex_push);
-    pthread_mutex_unlock(&head->push->mutex_next);
+  pthread_mutex_unlock(&head->push_lock);
+  pthread_mutex_unlock(&head->push->next_lock);
 
   return 0;
 }
@@ -48,42 +58,65 @@ int dqueue_push(dqueue_head *head, void *data, size_t data_size) {
 
 int dqueue_push_queue(dqueue_head *head, dqueue_head *push_head) {
 
-    /* It is important to always lock head before the link to avoid
-     * situations where two calling functions block each other */
-    pthread_mutex_lock(&head->mutex_push);
-    pthread_mutex_lock(&head->push->mutex_next);
+  /* It is important to always lock the head before the link to avoid
+   * situations where two calling functions block each other */
+  pthread_mutex_lock(&head->push_lock);
+  pthread_mutex_lock(&head->push->next_lock);
 
-    head->push->next = push_head->pop;
-    head->push = push_head->push;
+  head->push->next = push_head->pop;
+  head->push = push_head->push;
+  head->push->is_head = true;
 
-    pthread_mutex_unlock(&head->mutex_push);
-    pthread_mutex_unlock(&head->push->mutex_next);
+  pthread_mutex_unlock(&head->push_lock);
+  pthread_mutex_unlock(&head->push->next_lock);
 
-    free(push_head);
+  free(push_head);
 
   return 0;
 }
 
 
-int dqueue_pop(dqueue_head *head, void *data, size_t *data_size) {
+size_t dqueue_pop(dqueue_head *head, void *data) {
+  size_t data_size;
+  bool del_link = false;
+  pthread_mutex_t *p_lock;
+  prealloc_cell *p_cell;
+  prealloc_head *p_head;
 
-    pthread_mutex_lock(&head->mutex_pop);
+  pthread_mutex_lock(&head->pop_lock);
 
-    data = head->pop->data;
-    *data_size = head->pop->data_size;
-    free(head->pop);
-    head->pop = head->pop->next;
+  if (head->pop == NULL) {
+    pthread_mutex_unlock(&head->pop_lock);
+    return 0; }
 
-    pthread_mutex_unlock(&head->mutex_pop);
+  data_size = head->pop->data_size;
+  memcpy(data, head->pop->data, head->pop->data_size);
 
+  pthread_mutex_lock(&head->pop->num_poppers_lock);
 
+  head->pop->num_poppers--;
+  if ( head->pop->num_poppers <= 0 ) {
+    del_link = true;
+    p_lock = head->pop->prealloc.p_lock;
+    p_cell = head->pop->prealloc.p_cell;
+    p_head = head->pop->prealloc.p_head; }
 
-  return 0;
+  pthread_mutex_unlock(&head->pop->num_poppers_lock);
+
+  head->pop = head->pop->next;
+  pthread_mutex_unlock(&head->pop_lock);
+
+  if ( del_link == true ) {
+    pthread_mutex_lock(p_lock);
+    prealloc_del(p_head, p_cell);
+    pthread_mutex_unlock(p_lock); }
+
+  return data_size;
 }
 
 
 void dqueue_destroy(dqueue_head *head) {
-  free(head);
+  prealloc_destroy(head->p_head);
 }
 
 
--- dqueue.h
+++ dqueue.h
@@ -3,29 +3,53 @@
 
 #include <pthread.h>
 #include <stdbool.h>
+#include "prealloc.h"
+
+#define PREALLOC_ALLOC_SIZE 100
+#define PREALLOC_MAX_SIZE 10000
+
 
 
 typedef struct Dqueue_Head dqueue_head;
 typedef struct Dqueue_Link dqueue_link;
+typedef struct Dqueue_Prealloc dqueue_prealloc;
 typedef bool *dqueue_identifier;
 
+struct Dqueue_Prealloc {
+  pthread_mutex_t *p_lock;
+  prealloc_cell *p_cell;
+  prealloc_head *p_head;
+};
 
+/* 'is_head' must be first item in structure for dqueue_identifier
+ * to work. For Dqueue_Link, is_head is initially false
+ * and used for identification, but it may be true when
+ * denoting the start of a subqueue. */
+
+/* The origin of the link assign the number of destination
+ * threads to 'num_poppers', before the link. When a thread
+ * pops the link, also subtracts 1 from num_poppers. When
+ * (num_poppers == 0) the origin of the link may reuse link. */
 struct Dqueue_Link {
-  bool is_head;     // Must be first item in struct
+  bool is_head;
+  dqueue_prealloc prealloc;
   struct Dqueue_Link *next;
-  pthread_mutex_t mutex_next;
+  pthread_mutex_t next_lock;
+  unsigned int num_poppers;
+  pthread_mutex_t num_poppers_lock;
   size_t data_size;
   void *data;
 } __attribute__((packed));
 
 struct Dqueue_Head{
-  bool is_head;     // Must be first item in struct
-  bool is_blocked;
+  bool is_head;
+  prealloc_head *p_head;
+  pthread_mutex_t p_head_lock;
   dqueue_link *push;
   dqueue_link *pop;
-  pthread_mutex_t mutex_push;
-  pthread_mutex_t mutex_pop;
-};
+  pthread_mutex_t push_lock;
+  pthread_mutex_t pop_lock;
+} __attribute__((packed));
 
 
 
@@ -35,24 +59,35 @@ struct Dqueue_Head{
 /* DQUEUE_INIT - Initializes a queue.
  *
  *  dqueue_head *head     - A pointer to the queue that will be initialized.
+ *
+ *  Return:  0: Success
+ *          !0: Failure
  */
-dqueue_head *dqueue_init();
+int dqueue_init(dqueue_head *head);
 
 
 /* DQUEUE_PUSH - Push data into the queue.
  *
  *  dqueue_head *head     - A pointer to the queue that will be increased.
+ *  int32_t *num_poppers  - The number of threads that will use this link.
  *  void *data            - A pointer to the data that will be inserted.
  *  ssize_t data_size     - Size of the inserted data.
+ *
+ *  Return:  0: Success
+ *          !0: Failure
  */
-int dqueue_push(dqueue_head *head, void *data, size_t data_size);
+int dqueue_push(dqueue_head *head, int32_t num_poppers, void *data,
+    size_t data_size);
 
 
 
 /* DQUEUE_PUSH_QUEUE - Push a queue into another queue.
  *
  *  dqueue_head *head       - A pointer to the queue that will be increased.
- *  dqueue_head *push_head   - A pointer to the queue that will be inserted.
+ *  dqueue_head *push_head  - A pointer to the queue that will be inserted.
+ *
+ *  Return:  0: Success
+ *          !0: Failure
  */
 int dqueue_push_queue(dqueue_head *head, dqueue_head *push_head);
 
@@ -62,9 +97,10 @@ int dqueue_push_queue(dqueue_head *head, dqueue_head *push_head);
  *
  *  dqueue_head *head     - A pointer to the queue that will be reduced.
  *  dqueue_link *data     - A pointer to the data that will be retreived.
- *  ssize_t data_size     - Size of the retreived data.
+ *
+ * Return:  Size of the retreived data.
  */
-int dqueue_pop(dqueue_head *head, void *data, size_t *data_size);
+size_t dqueue_pop(dqueue_head *head, void *data);
 
 
 /* DQUEUE_DESTROY - Destroys a queue.
